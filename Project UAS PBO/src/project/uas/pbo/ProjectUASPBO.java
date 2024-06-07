4363fe91d0ccd0546653b99596468c7d4061631f
unknown <dalfanimerlia@gmail.com>
2024-06-07 11:15:27 +0700

    nambahkan file

---
+++ Project UAS PBO/src/project/uas/pbo/ProjectUASPBO.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project.uas.pbo;
+
+/**
+ *
+ * @author Dwi Alfani Merlia (222212575)
+ */
+public class ProjectUASPBO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
